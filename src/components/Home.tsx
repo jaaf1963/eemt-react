0c0fb581be43a47c70547bfe030351bf8c4d7193
jaaf1963 <juanalberto.albistur@gmail.com>
2026-01-28 16:49:59 -0300

    subida portadas2 28-01-2026

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,13 +4,40 @@ import PortadaGif from "./portadagif";
 const Home: React.FC = () => {
   //
   return (
-    <div className="container">
-      {/* Imagen de Fondo */}
-      <HomeImage />
+    <>
+      <div className="transparent-panel" style={{ width: "37%" }}>
+        <div className="container-transp">
+          <p style={{ fontSize: "21px", fontWeight: "bold" }}>
+            Empresa de Estudios Electromagnéticos
+          </p>
+        </div>
+      </div>
+      <div
+        className="transparent-panel"
+        style={{ width: "37%", marginTop: "5px" }}
+      >
+        <div className="container-transp" style={{}}>
+          <p style={{ fontSize: "20px" }}>Especialistas:</p>
+        </div>
+        <div className="container-transp" style={{ fontStyle: "italic" }}>
+          <p style={{ fontSize: "18px" }}>. Sistemas de Potencia</p>
+        </div>
+        <div className="container-transp" style={{ fontStyle: "italic" }}>
+          <p style={{ fontSize: "18px" }}>. Estudios por Simulación</p>
+        </div>
+        <div className="container-transp" style={{ fontStyle: "italic" }}>
+          <p style={{ fontSize: "18px" }}>. Pruebas en Sitio</p>
+        </div>
+      </div>
 
-      {/* GIF encima */}
-      <PortadaGif />
-    </div>
+      <div className="container">
+        {/* Imagen de Fondo */}
+        <HomeImage />
+
+        {/* GIF encima */}
+        <PortadaGif />
+      </div>
+    </>
   );
 };
 
